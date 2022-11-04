c5c2367ff412669f4259ca69d55cb93585795252
Saphir224 <taransafi@yahoo.ca>
2022-11-04 16:21:54 -0700

    Exercice with (while) version

--- Next_10_element.py
+++ Next_10_element.py
@@ -1,11 +1,24 @@
 #Write an algorithm that asks for a number and print the next 10 numbers
-
+    
+    
+    #For version
 first_number= float(input("Enter a number: "))
 index_me=0
 for index_me in range(10):
     index_me +=1 
     nombre_final= first_number + index_me
     print(nombre_final)
+
+    #While version 
+""" 
+numberEnter = float(input("Enter a lot number those you want: "))
+indexe_me = 1
+while indexe_me<=10:
+    final_num= numberEnter+indexe_me
+    indexe_me+=1
+    print(final_num) 
+    
+"""
 
 
 
