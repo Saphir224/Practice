0b6616fb8d9b99caf5b675423b48e2e2a9b04b65
Saphir224 <taransafi@yahoo.ca>
2023-03-28 22:31:25 -0700

    The consecutives numbers

---
+++ Consecutive_number.py
@@ -0,0 +1,16 @@
+
+
+"""integer_number= int(input("Enter the only integer number:  "))
+index_me = 0
+for index_me in range(integer_number):
+    index_me+= 1
+print(index_me)
+"""
+
+integer_number= int(input("Enter the only integer number:  "))
+indexe_me = 1
+sol=0
+while  indexe_me <= integer_number:
+    sol= indexe_me+sol 
+    indexe_me+=1 
+print(sol)
